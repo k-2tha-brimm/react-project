69a18cab1592aad9dea93e8ac47de128a30a0187
k-2tha-brimm <kev.brimm@gmail.com>
2019-06-26 19:47:14 -0700

    add conditional rendering to Media component. all items are videos, though!

--- app/src/Campaign.js
+++ app/src/Campaign.js
@@ -1,22 +1,12 @@
 import React from 'react';
-// import Media from './Media.js'
+import Media from './Media.js'
 import './Campaign.css';
 
 function Campaign(props) {
 
     let medias = (
         props.campaign.medias.map((media, idx) => (
-
-            <div className="the-container">
-                <div className="image-container">
-                    <img src={media.cover_photo_url} alt={props.campaign.campaign_name} height="200" width="120" />
-                    <i class="fas fa-play" id="play-btn"></i>
-                </div>
-                <div className="link-container">
-                    <a href={media.tracking_link} style={{borderRight: "1px solid rgb(238, 227, 227)"}}><i class="fas fa-link" id="link"></i></a>
-                    <a href={media.download_url}><i class="fas fa-download" id="link"></i></a>
-                </div>
-            </div>
+            <Media media={media} key={idx} />
         ))
     )
 
--- app/src/Media.js
+++ app/src/Media.js
@@ -2,14 +2,33 @@ import React from 'react';
 import './Media.css'
 
 function Media(props) {
-    console.log(props);
-    return (
-        <div className="media-container">
-            <div className="thumbnail">
-                <img src={props.media.cover_photo_url}></img>
+
+    if(props.media.media_type === "video") {
+        return (
+            <div className="the-container">
+                <div className="image-container">
+                    <img src={props.media.cover_photo_url} alt={props.media.download_url} height="200" width="120" />
+                    <i className="fas fa-play" id="play-btn"></i>
+                </div>
+                <div className="link-container">
+                    <a href={props.media.tracking_link} style={{borderRight: "1px solid rgb(238, 227, 227)"}}><i className="fas fa-link" id="link"></i></a>
+                    <a href={props.media.download_url}><i className="fas fa-download" id="link"></i></a>
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="the-container">
+                <div className="image-container">
+                    <img src={props.media.cover_photo_url} alt={props.media.download_url} height="200" width="120" />
+                </div>
+                <div className="link-container">
+                    <a href={props.media.tracking_link} style={{borderRight: "1px solid rgb(238, 227, 227)"}}><i class="fas fa-link" id="link"></i></a>
+                    <a href={props.media.download_url}><i class="fas fa-download" id="link"></i></a>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Media;
