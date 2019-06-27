3ecb9ecd8559a0afeb4ce5c6c62bcdc9780b4fdf
k-2tha-brimm <kev.brimm@gmail.com>
2019-06-26 19:39:43 -0700

    add play button overlay. conditional application incoming!

--- app/src/Campaign.js
+++ app/src/Campaign.js
@@ -6,8 +6,12 @@ function Campaign(props) {
 
     let medias = (
         props.campaign.medias.map((media, idx) => (
+
             <div className="the-container">
-                <img src={media.cover_photo_url} alt={props.campaign.campaign_name} height="200" width="120" />
+                <div className="image-container">
+                    <img src={media.cover_photo_url} alt={props.campaign.campaign_name} height="200" width="120" />
+                    <i class="fas fa-play" id="play-btn"></i>
+                </div>
                 <div className="link-container">
                     <a href={media.tracking_link} style={{borderRight: "1px solid rgb(238, 227, 227)"}}><i class="fas fa-link" id="link"></i></a>
                     <a href={media.download_url}><i class="fas fa-download" id="link"></i></a>
